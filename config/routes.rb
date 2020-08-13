be933e6d1e8f9adacd9092dd538ba29d11b7457c
arthur prats <arthurprats@gmail.com>
2020-08-13 15:16:32 -0400

    this is part 3 of ex 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/destroy'
+  get 'cocktails/index'
+  get 'cocktails/new'
+  get 'cocktails/create'
+  get 'cocktails/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :cocktails, only: [ :index, :show, :new, :create ] do
+    resources :doses, only: [ :new, :create, :destroy ]
+  end
 end
